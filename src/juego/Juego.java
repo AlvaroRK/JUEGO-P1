d66ad81c8869fee92f6905294cc0bb3f4d19a8b5
AlvaroRK <adriano.rk06@gmail.com>
2025-05-20 22:36:01 -0300

    1.1 Fondo agregado

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,48 +1,30 @@
 package juego;
 
 
-import java.awt.Color;
+import java.awt.*;
+import javax.swing.*;
 
 import entorno.Entorno;
 import entorno.InterfaceJuego;
 
-public class Juego extends InterfaceJuego
-{
-	// El objeto Entorno que controla el tiempo y otros
+public class Juego extends InterfaceJuego{
 	private Entorno entorno;
+	private Image fondo;
 	
-	// Variables y métodos propios de cada grupo
-	// ...
-	
-	Juego()
-	{
-		// Inicializa el objeto entorno
+	Juego(){
 		this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
 		
-		// Inicializar lo que haga falta para el juego
-		// ...
-
-		// Inicia el juego!
+		fondo = new ImageIcon(getClass().getResource("/imagenes/FondoTierra2D.png")).getImage();
 		this.entorno.iniciar();
 	}
 
-	/**
-	 * Durante el juego, el método tick() será ejecutado en cada instante y 
-	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
-	 * actualizar el estado interno del juego para simular el paso del tiempo 
-	 * (ver el enunciado del TP para mayor detalle).
-	 */
-	public void tick()
-	{
-		// Procesamiento de un instante de tiempo
-		// ...
-		
+	public void tick(){
+		entorno.dibujarImagen(fondo, 400, 300, 0);
 	}
 	
 
 	@SuppressWarnings("unused")
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 		Juego juego = new Juego();
 	}
 }
